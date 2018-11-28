af4ece0be7e3ed2caa40761d308bfbd4dc9ebe20
yawmiej <yomlateef@yahoo.com>
2018-11-28 23:33:19 +0100

    Added more questions with score

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-const question = `What is superman's real name`;
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+const quiz = [
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonderwoman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+]
+let score = 0;
+for (const [question, answer] of quiz){
+    const response = prompt(question);
+    if(response === answer) {
+        alert(`Correct!`);
+        score++;
+    } else {
+        alert(`Wrong, the correct answer was ${answer}`);
+    }
+}
+
+alert(`Game over, You scored ${score} point${score !== 1 ? 's' : ''}`);
