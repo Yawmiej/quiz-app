8c671c19e16a88a916d9d5afca04f21058f9a414
yawmiej <yomlateef@yahoo.com>
2018-11-29 23:46:18 +0100

    Improved answer and response matching

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 const quiz = [
     ["What is Superman's real name?", "Clark Kent"],
     ["What is Wonderwoman's real name?", "Diana Prince"],
-    ["What is Batman's real name?", "Bruce Wayne"]
-]
+    ["What is Batman's real name?", "Bruce Wayne"],
+    ["What is Iron Man's real name?", "Tony Stark"],
+    ["What is The Flash's real name", "Barry Allen"],
+    ["What is Captain America's real name", "Steve Rogers"]
+];
 let score = 0;
 for (const [question, answer] of quiz){
     const response = prompt(question);
-    if(response === answer) {
+    if(response.toLowerCase().trim() === answer.toLowerCase()) {
         alert(`Correct!`);
         score++;
     } else {
-        alert(`Wrong, the correct answer was ${answer}`);
+        alert(`Wrong, the correct answer was "${answer}"`);
     }
 }
 
