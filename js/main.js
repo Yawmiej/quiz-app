414c8006ce7d6341baa8bbf6257feab38b7aa8c0
yawmiej <yomlateef@yahoo.com>
2018-12-03 22:03:56 +0100

    Added functions for reusability

--- js/main.js
+++ js/main.js
@@ -6,15 +6,34 @@ const quiz = [
     ["What is The Flash's real name", "Barry Allen"],
     ["What is Captain America's real name", "Steve Rogers"]
 ];
-let score = 0;
-for (const [question, answer] of quiz){
-    const response = prompt(question);
-    if(response.toLowerCase().trim() === answer.toLowerCase()) {
-        alert(`Correct!`);
-        score++;
-    } else {
-        alert(`Wrong, the correct answer was "${answer}"`);
+function start(quiz){
+    let score = 0;
+
+    // main game loop
+    for (const [question, answer] of quiz) {
+        const response = ask(question);
+        check(response,answer);
+    }
+
+    gameOver();
+
+    function ask(question) {
+        return prompt(question);
+    }
+
+
+    function check(response, answer) {
+        if(response.toLowerCase().trim() === answer.toLowerCase()) {
+            alert(`Correct!`);
+            score++;
+        } else {
+            alert(`Wrong, the correct answer was "${answer}"`);
+        }
+    }
+
+    function gameOver() {
+        alert(`Game over, You scored ${score} point${score !== 1 ? 's' : ''}`);
     }
 }
 
-alert(`Game over, You scored ${score} point${score !== 1 ? 's' : ''}`);
+start(quiz);
